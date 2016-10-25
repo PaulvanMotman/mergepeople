3a2d032368a5b05d48276cd9f454ccf80d5c9bc9
Paul van Motman <pcvanmotman@gmail.com>
2016-10-25 12:43:00 +0200

    added comments

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
+// Requiring filesytem
 var fs = require('fs')
+
+// Creating three global variables to store the data of the files in
 var result1
 var result2
 var totalpeople 
 
+// Counting the files read to make sure the concatenating part of the function only triggers when
+// both files are read.
 var numFilesRead = 0;
+
+// Fucntion that concatenates files
 function complete() {
+  // Counter
   numFilesRead++
+  // If both files are read..
   if (numFilesRead === 2) {
+    // Add the two arrays together
     totalpeople = result1.concat(result2)
-    fs.writeFile('./peoplecomplete.txt', totalpeople.sort(), function (err) {
+    // Create a new file where we store a sorted version of the data of the array we just created
+    fs.writeFile('./people/peoplecomplete.txt', totalpeople.sort(), function (err) {
     	if (err) {
         	console.error('Following error is showing up: ' + err);
     	}
@@ -16,7 +27,8 @@ function complete() {
   }
 }
 
-fs.readFile('./people.json', function(err, data) {
+// Reading file 1
+fs.readFile('./people/people.json', function(err, data) {
 	if (err) {
 		throw err;
 	}
@@ -24,7 +36,8 @@ fs.readFile('./people.json', function(err, data) {
   complete();
 });
 
-fs.readFile('./people2.json', function(err, data) {
+// Reading file 2
+fs.readFile('./people/people2.json', function(err, data) {
 	if (err) {
 		throw err;
 	}
